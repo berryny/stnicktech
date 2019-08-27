083db2f220ba64ab4e63f9b17b0b9b03a968eeb5
berryny <judesire@yahoo.com>
2019-08-27 03:19:58 -0400

    footer copyright

--- js/styles.js
+++ js/styles.js
@@ -2,6 +2,18 @@
   "use strict";
   $(window).trigger('resize').trigger('scroll');
 
+  document.addEventListener('DOMContentLoaded', function() {
+    /* Using the new Date() function
+    new is a keyword which is required for Date
+    https://www.w3schools.com/js/js_date_methods.asp
+    */
+    var d = new Date();
+    var currentYear = d.getFullYear(); // dynamic data
+    /* Add copyright year */
+    var classYearElement = document.querySelector('footer .year');
+    classYearElement.innerHTML = currentYear;
+  });
+
   /*============================================
     COUNTER
   ==============================================*/
